cbc4effc11047c664638802a965b7a9257218d53
Stella <lottie798ko@gmail.com>
2026-05-27 12:17:24 +0630

    Add files via upload

--- VNSdroid/main.py
+++ VNSdroid/main.py
@@ -14,18 +14,27 @@ if platform != 'android':
 from modules.menu_logic import MainMenu
 from modules.game_interpreter import VNInterpreter
 from modules.settings_logic import SettingsMenu
+from modules.github import GithubMenu
+
+Builder.load_file('./assets/kv/menu.kv')
+Builder.load_file('./assets/kv/game.kv')
+Builder.load_file('./assets/kv/saveload.kv')
+Builder.load_file('./assets/kv/settings.kv')
+Builder.load_file('./assets/kv/github.kv')
 
 def set_android_orientation(orientation='portrait'):
     if platform == 'android':
-        from jnius import autoclass
-        PythonActivity = autoclass('org.kivy.android.PythonActivity')
-        Activity = PythonActivity.mActivity
-        ActivityInfo = autoclass('android.content.pm.ActivityInfo')
-        
-        if orientation == 'landscape':
-            Activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
-        else:
-            Activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        try:
+            from jnius import autoclass
+            PythonActivity = autoclass('org.kivy.android.PythonActivity')
+            Activity = PythonActivity.mActivity
+            ActivityInfo = autoclass('android.content.pm.ActivityInfo')
+            if orientation == 'landscape':
+                Activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+            else:
+                Activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        except Exception as e:
+            pass
 
 class MainMenuScreen(Screen):
     def on_pre_enter(self, *args):
@@ -51,17 +60,21 @@ class SettingsScreen(Screen):
         self.settings_widget = SettingsMenu()
         self.add_widget(self.settings_widget)
 
+class GithubScreen(Screen):
+    def on_pre_enter(self, *args):
+        set_android_orientation('portrait')
+
+    def on_kv_post(self, base_widget):
+        self.github_widget = GithubMenu()
+        self.add_widget(self.github_widget)
+
 class VNSdroidApp(App):
     def build(self):
-        Builder.load_file('./assets/kv/menu.kv')
-        Builder.load_file('./assets/kv/game.kv')
-        Builder.load_file('./assets/kv/saveload.kv') 
-        Builder.load_file('./assets/kv/settings.kv') 
-        
         sm = ScreenManager()
         sm.add_widget(MainMenuScreen(name='menu_screen'))
         sm.add_widget(GameScreen(name='game_screen'))
         sm.add_widget(SettingsScreen(name='settings_screen'))
+        sm.add_widget(GithubScreen(name='github_screen'))
         return sm
 
 if __name__ == '__main__':
